39b808b98a9f8ce3026fd25e70998aac8ae2ae2a
Alpha <alphatero@icloud.com>
2020-12-09 15:56:38 +0800

    modal added

--- prj2/src/components/pages/Products.vue
+++ prj2/src/components/pages/Products.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="text-right mt-4">
-      <button class="btn btn-primary">建立新的產品</button>
+      <button class="btn btn-primary" @click="openModal">
+        建立新的產品
+      </button>
     </div>
     <table class="table mt-4">
       <thead>
@@ -15,7 +17,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item) in products" :key="item.id">
+        <tr v-for="item in products" :key="item.id">
           <td>{{ item.category }}</td>
           <td>{{ item.title }}</td>
           <td class="text-right">
@@ -34,10 +36,37 @@
         </tr>
       </tbody>
     </table>
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery';
 export default {
   data() {
     return {
@@ -54,9 +83,12 @@ export default {
         vm.products = response.data.products;
       });
     },
+    openModal(){
+      $('#productModal').modal('show');
+    }
   },
   created() {
-      this.getProducts();
+    this.getProducts();
   },
 };
 </script>
