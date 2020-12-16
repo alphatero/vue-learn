9b961911f9207fa89e88d2a64d5c4f6c933c97ec
Alpha <alphatero@icloud.com>
2020-12-16 13:39:25 +0800

    pagination component added
    but have navbar not fixed issue!

--- prj2/src/components/pages/Products.vue
+++ prj2/src/components/pages/Products.vue
@@ -43,23 +43,7 @@
         </tr>
       </tbody>
     </table>
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item" :class="{'disabled': !pagination.has_pre}">
-          <a class="page-link" href="#" aria-label="Previous" @click.prevent="getProducts(pagination.current_page - 1)">{{ page }}
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li class="page-item" v-for="page in pagination.total_pages" :key="page" :class="{'active': pagination.current_page === page}">
-          <a class="page-link" href="#" @click.prevent="getProducts(page)">{{ page }}</a>
-          </li>
-        <li class="page-item" :class="{'disabled': !pagination.has_next}">
-          <a class="page-link" href="#" aria-label="Next" @click.prevent="getProducts(pagination.current_page + 1)">{{ page }}
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+<Pagination :pages="pagination" @emitPages="getProducts" />
     <!-- Modal -->
     <div
       class="modal fade"
@@ -261,7 +245,11 @@
 
 <script>
 import $ from "jquery";
+import Pagination from '../pagination.vue';
 export default {
+  components: {
+     Pagination, 
+     },
   data() {
     return {
       products: [],
