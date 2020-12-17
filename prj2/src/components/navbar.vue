aafe2219996f3cbbbda665f46c3f92cc64890dda
Alpha <alphatero@icloud.com>
2020-12-17 08:13:12 +0800

    fix navbar fixed top

--- prj2/src/components/navbar.vue
+++ prj2/src/components/navbar.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <nav
-      class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
-    >
+     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
       <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#"
         >Company name</a
       >
